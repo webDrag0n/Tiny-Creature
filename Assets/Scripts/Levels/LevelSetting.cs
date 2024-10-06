65341f61a9a546476886c4df4b0940961c81db4f
Honnen <xhn2333@foxmail.com>
2024-10-06 23:44:33 +0800

    [src] LevelSetting updated

--- Assets/Scripts/Levels/LevelSetting.cs
+++ Assets/Scripts/Levels/LevelSetting.cs
@@ -11,10 +11,28 @@ public class LevelSetting : ScriptableObject
     }
 
     public int level_index;
+
+    public int num_level;
+    public float time_limit;
+    public float generate_speed = 1;
+
     public LevelColor[] level_color;
 
-    public float[] lambda_common;
-    public float[] lambda_high_priority;
-    public float[] lambda_boss;
+    public int num_of_high_priority;    // -1 represent inf
+    public int num_of_boss;             // -1 represent inf
 
+    public float lambda_common;
+    public float lambda_high_priority;
+    public float lambda_boss;
+
+    public enum GenerateMethod
+    {
+        UNIFORM,
+        EXPONENTIAL,
+        AFTER_HALF,
+    }
+
+    public GenerateMethod generate_method_common = GenerateMethod.UNIFORM;
+    public GenerateMethod generate_method_high_priority;
+    public GenerateMethod generate_method_boss;
 }
